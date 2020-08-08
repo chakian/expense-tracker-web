b8cfebf5f04b9f59ded75951db7f4e3c29e0e109
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-08-08 21:40:36 +0300

    ne olacaksa olsun

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,3 @@
 Write-Output "Starting config manipulation"
 
-.\replace_appsettings.ps1 -config 'C:\projects\expense-tracker-web\src\WebUI\appsettings.json'
-.\replace_appsettings.ps1 -config "projects\expense-tracker-web\src\WebUI\appsettings.json"
-.\replace_appsettings.ps1 -config "C:/projects/expense-tracker-web/src/WebUI/appsettings.json"
-.\replace_appsettings.ps1 -config "./projects/expense-tracker-web/src/WebUI/appsettings.json"
+.\replace_appsettings.ps1 -config C:\projects\expense-tracker-web\src\WebUI\appsettings.json
