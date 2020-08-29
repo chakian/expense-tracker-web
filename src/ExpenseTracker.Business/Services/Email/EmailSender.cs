e81c19e64ab54c2bd9337f31d1eca8d9ca79c98a
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-08-29 12:00:17 +0300

    SendGrid - set from email

--- src/ExpenseTracker.Business/Services/Email/EmailSender.cs
+++ src/ExpenseTracker.Business/Services/Email/EmailSender.cs
@@ -25,7 +25,7 @@ namespace ExpenseTracker.Business.Services.Email
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("Joe@contoso.com", Options.SendGridUser),
+                From = new EmailAddress("cagdaskorkut@gmail.com", Options.SendGridUser),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
