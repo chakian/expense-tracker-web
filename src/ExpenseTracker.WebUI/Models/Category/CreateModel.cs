7d0d3a6a6d2270347aa82367d108c31871521889
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-09-30 14:51:41 +0300

    Category models created... Dummy models.

---
+++ src/ExpenseTracker.WebUI/Models/Category/CreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.WebUI.Models.Category
+{
+    public class CreateModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
