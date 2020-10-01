05327899054d67396c27dc374baf161d280f1729
Çağdaş Korkut <cagdaskorkut@hotmail.com>
2020-10-01 15:04:24 +0300

    added transaction db object class

---
+++ src/ExpenseTracker.Persistence/DbModels/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Persistence.DbModels
+{
+    public class Transaction : BaseAuditableDbo
+    {
+        [Required]
+        public int AccountId { get; set; }
+        public virtual Account Account { get; set; }
+
+        public int TargetAccountId { get; set; }
+        public Account TargetAccount { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
