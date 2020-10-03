da378ae7ee326a10d1c0534d079a203ceb7fb5f7
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-10-03 19:42:13 +0300

    Transaction Management. Done. With some flaws :)

---
+++ src/ExpenseTracker.WebUI/Models/Transaction/DeleteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExpenseTracker.WebUI.Models.Transaction
+{
+    public class DeleteModel
+    {
+        public int Id { get; set; }
+        public int BudgetId { get; set; }
+        public DateTime Date { get; set; }
+        public int AccountId { get; set; }
+        //public int? TargetAccountId { get; set; }
+        //public bool IsSplitTransaction { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+    }
+}
