a20da399f044a0e9eb9354d7c17b9b9954bf3e01
Çağdaş Korkut <cagdaskorkut@hotmail.com>
2020-10-03 18:14:45 +0300

    Implemented basic CRUD for Category Management

--- src/ExpenseTracker.WebUI/Models/Category/CreateModel.cs
+++ src/ExpenseTracker.WebUI/Models/Category/CreateModel.cs
@@ -3,7 +3,6 @@
     public class CreateModel
     {
         public string Name { get; set; }
-        public string Type { get; set; }
-        public decimal Balance { get; set; }
+        //public int ParentCategoryId { get; set; }
     }
 }
--- src/ExpenseTracker.WebUI/Models/Category/ListModel.cs
+++ src/ExpenseTracker.WebUI/Models/Category/ListModel.cs
@@ -10,8 +10,6 @@ namespace ExpenseTracker.WebUI.Models.Category
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            public string Type { get; set; }
-            public decimal Balance { get; set; }
         }
     }
 }
