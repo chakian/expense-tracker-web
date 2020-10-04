f5cc163479672f5979b3552aa1ae12e33d1bd9e3
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-10-04 12:29:23 +0300

    added UseKestrel to host builder

--- src/ExpenseTracker.WebUI/Program.cs
+++ src/ExpenseTracker.WebUI/Program.cs
@@ -8,22 +8,23 @@ namespace ExpenseTracker.WebUI
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
         }
 
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseStartup<Startup>();
+                });
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options => options.AddServerHeader = false)
-                .UseStartup<Startup>();
-        }
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        //{
+        //    return WebHost.CreateDefaultBuilder(args)
+        //        .UseKestrel(options => options.AddServerHeader = false)
+        //        .UseStartup<Startup>();
+        //}
     }
 }
