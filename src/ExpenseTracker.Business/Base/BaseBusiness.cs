7d2c7b7869e7e8e811e3448c1df85fcbb2967a7b
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-11-29 13:10:42 +0300

    BaseBusiness only takes context. BaseBusiness takes care of inserttime and updatetime in dbo

--- src/ExpenseTracker.Business/Base/BaseBusiness.cs
+++ src/ExpenseTracker.Business/Base/BaseBusiness.cs
@@ -1,14 +1,32 @@
 ﻿using ExpenseTracker.Persistence;
-using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Persistence.DbModels;
+using System;
 
 namespace ExpenseTracker.Business.Base
 {
     public class BaseBusiness
     {
         protected readonly ExpenseTrackerDbContext context;
-        public BaseBusiness(DbContextOptions<ExpenseTrackerDbContext> _options)
+        public BaseBusiness(ExpenseTrackerDbContext _context)
         {
-            context = new ExpenseTrackerDbContext(_options);
+            context = _context;
+        }
+
+        protected T CreateNewAuditableObject<T>(string userId, bool isActive = true)
+            where T : BaseAuditableDbo, new()
+        {
+            T dbo = new T();
+            dbo.InsertUserId = userId;
+            dbo.InsertTime = DateTime.UtcNow;
+            dbo.IsActive = isActive;
+            return dbo;
+        }
+
+        protected void UpdateAuditableObject<T>(T dbo, string userId, bool isActive = true)
+            where T : BaseAuditableDbo
+        {
+            dbo.UpdateUserId = userId;
+            dbo.UpdateTime = DateTime.UtcNow;
         }
     }
 }
